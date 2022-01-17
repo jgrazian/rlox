7638168d67405ae27501c873b00a3eb9432be4f0
jgrazian <34037413+jgrazian@users.noreply.github.com>
2022-01-17 22:34:21 +0000

    Deploying to gh-pages from @ jgrazian/rlox@5801843e1e692b063db5d307e055a451ee40308d 🚀

---
+++ rlox/rlox_lib.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} source
+*/
+export function run(source: string): void;
---
+++ rlox/rlox_lib.js
@@ -0,0 +1,2 @@
+import * as wasm from "./rlox_lib_bg.wasm";
+export * from "./rlox_lib_bg.js";
